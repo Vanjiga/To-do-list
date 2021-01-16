b598f7d55219e45d3ab9af93eb66b612db529785
Vanjiga <vanja.gacic123@gmal.com>
2021-01-16 12:56:00 +0100

    first proper output
    proper to do list output

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 
 A=[]
+i=0
 
-def ListToString(s):
-    s = ""
-    return (s.join(A))
+
 
 to_do = input(str("mode program? new/edit"))
 if to_do == "new":
@@ -12,17 +11,21 @@ if to_do == "new":
         print ('page',(i+1))
         k = input('')
         A.append(k)
-        print(A) 
 
-        ending = input(str("save? y/n"))
-        if ending == "y":
-            file = open("To-do.txt","a")
-            ListToString(s)
-            file.write(s)
-            file.close()
-            print("saved")
+    output = ("\n".join(A))
+    print ("\n".join(A))
+    ending = input(str("save? y/n"))
+    if ending == "y":
+        filename = input(str("Filename"))
+        file = open(filename +".txt","a")
+        file.write(output)
+        file.close()
+        print("saved")
 
 
 else:
-    print("cya")
+    print("edit mode")
+    to_edit=input(str("File name?"))
+    file = open(to_edit + ".txt", "r")
+    print(file)
 
